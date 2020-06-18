44704967e8032d7c75371c3d5e4d7b9ed482959a
David <->
2020-06-18 12:31:04 +0100

    lunch

--- machine.go
+++ machine.go
@@ -24,11 +24,14 @@ const (
 
 func reset(m *machine) {
 	m.pc = 0
+	m.nstack = 0
+	m.nret = 0
 	m.err = OK
 }
 
 // pre: stack not empty
-func pop (m *machine) word {
+// this is the internal helper function
+func _pop (m *machine) word {
 	if m.nstack == 0 {
 		panic("pop on empty stack")
 	}
@@ -37,22 +40,90 @@ func pop (m *machine) word {
 	return x
 }
 
-func push(m *machine, x word) uint8 {
+// this is an internal helper, not the instruction
+func _push(m *machine, x word) {
 	if m.nstack > 255 {
-		return OVERFLOW
+		panic("stack overflow")
 	}
-	m.nstack++
 	m.stack[m.nstack] = x
-	return OK
+	m.nstack++
 }
+
+func peek(m *machine) word {
+	if m.nstack == 0 {
+		panic("peek on empty stack")
+	}
+	return m.stack[m.nstack - 1]
+}
+
+func step (m *machine) {
+	// fetch
+	var instruction word = m.code[m.pc]
+	m.pc++
+	// decode
+	var f, err = decode(instruction)
+	if err != OK {
+		m.err = ILLEGAL
+		return
+	}
+	// execute
+	result := f(m)
+	if result != OK {
+		m.err = result
+	}
+}
+
+// instructions //
 
 func add (m *machine) uint8 {
 	if m.nstack < 2 {
 		m.err = UNDERFLOW
 		return m.err
 	}
-	var y word = pop(m)
-	var x word = pop(m)
+	var y word = _pop(m)
+	var x word = _pop(m)
 	x = x + y
-	return push(m, x)
+	// cannot fail, as we just popped two
+	_push(m, x)
+	return OK
+}
+
+// this is the push instruction
+func push (m *machine) uint8 {
+	if m.nstack > 255 {
+		m.err = OVERFLOW
+		return m.err
+	}
+	var x word = m.code[m.pc]
+	m.pc++
+	// this is safe, as checked above
+	_push(m, x)
+	return OK
+}
+
+// this is the instruction
+func pop (m *machine) uint8 {
+	if m.nstack == 0 {
+		m.err = UNDERFLOW
+		return m.err
+	}
+	m.nstack--
+	return OK
+}
+
+// the decoding table //
+
+var INSTRUCTIONS = map[word] func(*machine) uint8 {
+	0x0001: push,
+	0x0002: pop,
+
+	0x0010: add,
+}
+
+func decode(instruction word) (func(*machine) uint8, uint8) {
+	f, exists := INSTRUCTIONS[instruction]
+	if exists == false {
+		return nil, ILLEGAL
+	}
+	return f, OK
 }
---
+++ main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func load_program(m *machine, p []word) {
+    for i := 0; i < len(p); i++ {
+        m.code[i] = p[i]
+    }
+}
+
+func TestReset(t *testing.T) {
+    var M machine
+    m := &M
+    reset(m)
+}
+
+func TestPushPop(t *testing.T) {
+    var M machine
+    m := &M
+    reset(m)
+    if m.nstack != 0 {
+        t.Errorf("stack not empty")
+    }
+    // push 0x2002; push 0x3003; pop
+    load_program(m, []word{0x0001, 0x2002, 0x0001, 0x3003, 0x0002})
+	
+	step(m)
+	assert.Equal(t, word(0x0001), m.nstack)
+	assert.Equal(t, word(0x2002), m.stack[m.nstack-1])
+	assert.Equal(t, word(2), m.pc)
+
+	step(m)
+	assert.Equal(t, word(0x0002), m.nstack)
+	assert.Equal(t, word(0x3003), m.stack[m.nstack-1])
+	assert.Equal(t, word(4), m.pc)
+
+	step(m)
+	assert.Equal(t, word(0x0001), m.nstack)
+	assert.Equal(t, word(5), m.pc)
+}
+
+func TestAdd(t *testing.T) {
+	var M machine
+    m := &M
+	reset(m)
+	// push 2; push 3; add
+	load_program(m, []word{0x0001, 0x0002, 0x0001, 0x0003, 0x0010})
+	step(m)
+	step(m)
+	step(m)
+	assert.Equal(t, word(1), m.nstack)
+	assert.Equal(t, word(5), peek(m))
+}
