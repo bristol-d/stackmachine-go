5cdd7fd31c00a584b23a10a4bee822bedeeba1b0
David <->
2020-06-19 10:03:21 +0100

    wasm start

---
+++ tinybuild.bat
@@ -0,0 +1,9 @@
+@rem build the wasm module with tinygo
+@setlocal
+@cd wasm
+@set GOROOT=C:\Go
+@echo Building wasm ...
+tinygo build -o go-stackmachine.wasm -target wasm ./wasm-main.go
+@if exist go-stackmachine.wasm (
+    copy go-stackmachine.wasm ..\web
+)
