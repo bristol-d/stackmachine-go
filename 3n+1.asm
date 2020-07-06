c32646c894009eccc02f05ca3eeb132292563de9
David <->
2020-07-06 10:56:17 +0100

    write to memory in 3n+1 example

--- 3n+1.asm
+++ 3n+1.asm
@@ -2,10 +2,21 @@
 ; given a number N, repeat until you reach 1:
 ; if N is even, replace with N/2
 ; if N is odd, replace with 3N + 1
-; an interrupt occurs ebfore every number in the sequence
+; put all the numbers encountered on the way in data memory
+; starting at location 1, with location 0 used as a pointer
+; an optional interrupt occurs before every number in the sequence
 
+            PUSH #1     ; initialise data pointer to 1
+            STOR #0
             PUSH #7     ; the original N
-start:      INT
+start:      DUP         ; store in data memory
+            LOAD #0
+            STRS
+            LOAD #0     ; increase pointer in data[0]
+            PUSH #1
+            ADD
+            STOR #0
+            ; INT         ; interrupt - enable if needed
             DUP         ; make a copy for testing if it's 1
             PUSH #1
             CEQ
